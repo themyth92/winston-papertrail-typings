83488c591d18738fe63e36473962ead119824fc3
paul <themyth92@gmail.com>
2019-08-14 14:06:50 +0800

    Fix module problem

--- index.d.ts
+++ index.d.ts
@@ -1,32 +1,31 @@
-import * as winston from 'winston';
-import * as Transport from 'winston-transport';
-
-interface PapertrailTransportOptions {
-  host: string
-  port: number
-  disableTls: boolean
-  level: keyof winston.config.AbstractConfigSetLevels
-  levels: winston.config.AbstractConfigSetLevels
-  hostname: string
-  program: string
-  facility: string
-  logFormat: string
-  colorize: boolean
-  inlineMeta: boolean
-  handleExceptions: boolean
-  flushOnClose: boolean
-  depth: number
-  attemptsBeforeDecay: number
-  maximumAttempts: number
-  connectionDelay: number
-  maxDelayBetweenReconnection: number
-  maxBufferSize: number
-}
-
-interface PapertrailTransportInstance extends Transport {
-  new(options?: PapertrailTransportOptions): PapertrailTransportInstance
-}
-
 declare module 'winston-papertrail' {
+  import * as winston from 'winston';
+  import * as Transport from 'winston-transport';
+
+  interface PapertrailTransportOptions {
+    host: string
+    port: number
+    disableTls: boolean
+    level: keyof winston.config.AbstractConfigSetLevels
+    levels: winston.config.AbstractConfigSetLevels
+    hostname: string
+    program: string
+    facility: string
+    logFormat: string
+    colorize: boolean
+    inlineMeta: boolean
+    handleExceptions: boolean
+    flushOnClose: boolean
+    depth: number
+    attemptsBeforeDecay: number
+    maximumAttempts: number
+    connectionDelay: number
+    maxDelayBetweenReconnection: number
+    maxBufferSize: number
+  }
+
+  interface PapertrailTransportInstance extends Transport {
+    new(options?: PapertrailTransportOptions): PapertrailTransportInstance
+  }
   const Papertrail: PapertrailTransportInstance
 }
